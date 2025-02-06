02ecada16ec68682c7590484f87ccabc458e22ba
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2025-02-06 14:35:37 -0500

    first commit

---
+++ src/constants/constants.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { OAuth2Client } from "google-auth-library";
+
+dotenv.config();
+
+export const PORT = process.env.PORT || ("8080" as string);
+export const URL_DATABASE =
+  process.env.URL_DATABASE || ("url of your data base" as string);
+export const TOKEN_SECRET_KEY =
+  process.env.TOKEN_SECRET_KEY! || ("your secret key" as string);
+
+export const CLIENT_ID = process.env.CLIENT_ID as string;
+export const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
+export const REDIRECT_URI = process.env.REDIRECT_URI as string;
+
+export const googleClient = new OAuth2Client(CLIENT_ID);
