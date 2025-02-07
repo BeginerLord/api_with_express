d3d41f654b3e97841332dd38b542a63a34169735
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2025-02-07 14:36:47 -0500

    Add JWT token generation and payload interface

---
+++ src/security/jwt/TokenPayload.ts
@@ -0,0 +1,5 @@
+export interface TokenPayload {
+    uuid: string;
+    email: string,
+
+  }
---
+++ src/security/jwt/generateToken.ts
@@ -0,0 +1,7 @@
+import { TOKEN_SECRET_KEY } from "@/constants"
+import { TokenPayload } from "./TokenPayload"
+import jwt from "jsonwebtoken"
+
+export const generateToken = (payload: TokenPayload): string => {
+    return jwt.sign(payload, TOKEN_SECRET_KEY, { expiresIn: '1h' })
+  }
