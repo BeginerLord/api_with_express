4210671842bfb479f4893ba6285cb1d3c11336c6
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2025-02-25 21:47:46 -0500

    Enhance authorization middleware: validate user object and improve token handling; update TypeScript configuration for type roots

--- src/middlewares/auth.midleware.ts
+++ src/middlewares/auth.midleware.ts
@@ -1,9 +1,9 @@
 import { getModel } from "@/config/database";
 import { Collection } from "@/constants";
-import { RolType, UserSchemaMongo } from "@/entities";
+import { User, UserSchemaMongo } from "@/entities";
 import { verifyToken } from "@/security";
 import { setError } from "@/utils/errors";
-import { NextFunction, RequestHandler, Request, Response } from "express";
+import { RequestHandler } from "express";
 
 export const authorize: RequestHandler = async (req, _res, next) => {
   try {
@@ -21,21 +21,29 @@ export const authorize: RequestHandler = async (req, _res, next) => {
     if (!validateToken || !validateToken.uuid) {
       return next(setError(401, "Not authorized - invalid token"));
     }
-
-    const user = await model.findOne({ uuid: validateToken.uuid });
+    const user = await model.findOne({ uuid: validateToken.uuid }).lean();
     if (!user) {
       return next(setError(404, "User not found"));
     }
+    // Validar que el objeto tiene todas las propiedades necesarias
+    const validatedUser = user as unknown as User;
+    if (
+      !validatedUser.uuid ||
+      !validatedUser.name ||
+      !validatedUser.email ||
+      !validatedUser.password ||
+      !validatedUser.rol // Asegúrate de que el rol esté presente
+    ) {
+      return next(setError(500, "Invalid user object"));
+    }
 
-    (req as any).user = user;
+    req.user = validatedUser; // Asigna el usuario decodificado a 'req.user'
 
     next();
   } catch (error) {
     return next(setError(401, `Not authorized -> ${(error as Error).message}`));
   }
 };
-
-
 
 const extractToken = (authHeader: string): string => {
   return authHeader.split(" ")[1];
